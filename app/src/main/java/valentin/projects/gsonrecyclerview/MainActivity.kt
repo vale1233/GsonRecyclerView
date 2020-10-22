2f52bba31f110371f612da646314c9bde62b76fb
Valentin <vkovacic8@gmail.com>
2020-10-22 10:32:36 +0200

    Initial commit

---
+++ app/src/main/java/valentin/projects/gsonrecyclerview/MainActivity.kt
@@ -0,0 +1,53 @@
+package valentin.projects.gsonrecyclerview
+
+import android.graphics.Color
+import android.graphics.Color.BLUE
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //recyclerView_main.setBackgroundColor(BLUE)
+
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        //recyclerView_main.adapter = MainAdapter()
+
+        fetchJson()
+
+    }
+
+    fun fetchJson() {
+
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback{
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+                val homeFeed = gson.fromJson(body, HomeFeed::class.java)
+
+                runOnUiThread {
+                    recyclerView_main.adapter = MainAdapter(homeFeed)
+                }
+
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed")
+            }
+        })
+
+    }
+
+}
---
+++ app/src/main/java/valentin/projects/gsonrecyclerview/MainAdapter.kt
@@ -0,0 +1,43 @@
+package valentin.projects.gsonrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.video_row.view.*
+
+class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
+
+    val videoTitles = listOf("First", "2nd", "3rd")
+
+    //number of items
+    override fun getItemCount(): Int {
+        return homeFeed.videos.count()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
+        return CustomViewHolder(cellForRow)
+    }
+
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        //val videoTitle = videoTitles.get(position)
+        val video = homeFeed.videos.get(position)
+        holder?.view?.textView_video_title?.text = video.name
+        holder?.view?.textView_channel_name?.text = video.channel.name
+
+        val avatar = holder?.view?.imageView_avatar
+        Picasso.with(holder?.view?.context).load(video.imageUrl).into(avatar)
+
+        val profile = holder?.view?.imageView_profile
+        Picasso.with(holder?.view?.context).load(video.channel.profileImageUrl).into(profile)
+    }
+
+}
+
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+}
